d40bc8e7ac0126211f4527da1b8c019b94031fc5
Martin Denis <martin.2nis@gmail.com>
2023-11-30 16:15:39 +0100

    Removed back link

--- index.php
+++ index.php
@@ -2,16 +2,11 @@
 <!DOCTYPE html>
 <html lang="<?php echo $language_iso; ?>">
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <link rel="icon" type="image/svg" href="<?php echo $favicon_path; ?>favicon.svg" />
-    <link rel="stylesheet" type="text/css" href="<?php echo $css_path; ?>game.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="./index.css" media="screen" />
+    <?php include_once '../../_games_head.php'; ?>
     <title>Scratch game</title>
   </head>
 
   <body>
-    <a href="<?php echo $sandbox_path; ?>" id="back">Back</a>
     <div id="container">
       <h1>Scratch game</h1>
       <div id="content">
