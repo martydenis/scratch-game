5fc221134f781b5e8e16b9e5fba62d904b1680b1
martydenis <martin.2nis@gmail.com>
2024-09-01 14:10:18 +0200

    [IMP] Simplified relative URLs

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <base href="<?php echo !empty($route['path']) ? '/' . $route['path'] : './' ?>">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet" media="screen">
-        <link rel="icon" type="image/svg" href="../martindenis/favicon.svg" />
+        <link rel="icon" type="image/svg" href="/favicon.svg" />
         <link rel="stylesheet" type="text/css" href="./index.css" media="screen" />
-        <title>Pew Pew</title>
+        <title>Scratch Game</title>
     </head>
     <body>
         <div id="container">
@@ -16,12 +17,12 @@
             <div id="content">
                 <canvas id="canvas" width="400" height="400"></canvas>
                 <canvas id="canvas__shadow" width="400" height="400"></canvas>
-                <img id="canvas__bg" src="./bg.jpg" alt="mask" width="600" height="600">
-                <img id="canvas__brush" src="./brush.svg" alt="mask" width="50" height="50">
+                <img id="canvas__bg" src="./img/bg.jpg" alt="mask" width="600" height="600">
+                <img id="canvas__brush" src="./img/brush.svg" alt="mask" width="50" height="50">
             </div>
         </div>
 
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script src="./index.js"></script>
+        <script src="./lib/jquery-3.5.1.min.js"></script>
+        <script type="module" src="./index.js"></script>
     </body>
 </html>
